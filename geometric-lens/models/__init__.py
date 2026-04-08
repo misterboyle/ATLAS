123081de0e20964a2ceb6c17ae0a3798f4dbda39
Xerxes <xerxes@cogitor.local>
2026-04-08 01:06:41 -0400

    fix: add stub models/ dir for geometric-lens Dockerfile COPY

---
+++ geometric-lens/models/__init__.py
@@ -0,0 +1,3 @@
+# models/ -- stub package (upstream v3.0.1 data model definitions)
+# Created to satisfy Dockerfile COPY models/ models/
+# TODO: restore full model definitions from upstream if needed
